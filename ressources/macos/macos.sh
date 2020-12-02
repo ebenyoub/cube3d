a1c830b0a0a5a2b62d95ab036217aa3ebb416441
Elyas Benyoub <ebenyoub@me.com>
2020-12-02 17:16:44 +0100

    corriger red cross linux 33/17, 1L << 17 | script makefile

--- ressources/macos/macos.sh
+++ ressources/macos/macos.sh
@@ -1,3 +1,4 @@
-	cp ressources/macos/keycode.h includes
-	cp ressources/macos/cub3d.h includes
+	cp ressources/macos/keycode.h includes/
+	cp ressources/macos/cub3d.h includes/
 	cp ressources/macos/key_init.c sources/
+	cp ressources/macos/cub_main.c sources/
